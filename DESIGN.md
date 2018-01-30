a07c41118546dfaa226012efaa99a982b6c9a0af
Sean Dague <sean@dague.net>
2018-01-30 15:48:46 -0500

    updated to latest almost working state

---
+++ DESIGN.md
@@ -0,0 +1,12 @@
+# Design for NY Power API
+
+Data is available in CSV files here, updated at 5 minute increments -
+http://mis.nyiso.com/public/
+
+Import DATA to Data Store
+
+
+bx cr build -t registry.ng.bluemix.net/sdague/ny-power-mosquitto
+images/mosquitto_server
+
+Push to
---
+++ images/ny-power-pump/process.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import csv
+import datetime
+import json
+import logging
+import io
+import time
+import urllib.request
+
+import paho.mqtt.client as mqtt
+
+FUEL_MIX="http://mis.nyiso.com/public/csv/rtfuelmix/{0}rtfuelmix.csv"
+
+_LOGGER = logging.getLogger(__name__)
+logging.basicConfig(ormat='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG)
+
+HOST = "ny-power-mosquitto"
+
+def collect_data():
+    now = datetime.datetime.now()
+    url = FUEL_MIX.format(now.strftime("%Y%m%d"))
+
+    # unfortunately we can't quite connect urllib to csv
+    with urllib.request.urlopen(url) as response:
+        out = io.StringIO()
+        out.write(response.read().decode('utf-8'))
+
+    # We have to rewind the output stream so it can be read by
+    # csv.reader
+    out.seek(0)
+    reader = csv.reader(out, quoting=csv.QUOTE_NONE)
+    data = []
+    last = ""
+    for row in reader:
+        last = row[0]
+        data.append(row)
+
+    return data
+
+def send_last_to_mqtt(data, last_sent=""):
+    client = mqtt.Client(clean_session=True)
+    client.connect(HOST)
+
+    last = ""
+    for r in data:
+        last = r[0]
+
+    retval = None
+    for r in data:
+        if r[0] == last and r[0] != last_sent:
+            print("Found new data to publish: %s" % r)
+            client.publish("ny-power/fuel-mix/{0}".format(r[2]),
+                           json.dumps(dict(ts=r[0], power=r[3], units="kW")))
+            retval = r[0]
+    if retval is not None:
+        client.publish("ny-power/updated/fuel-mix",
+                       json.dumps(dict(ts=r[0])), retain=True)
+    client.disconnect()
+    return retval
+
+
+def main():
+    last = ""
+    while(True):
+        data = collect_data()
+        ret_last = send_last_to_mqtt(data, last)
+        if ret_last is not None:
+            last = ret_last
+        print("Sleeping")
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
