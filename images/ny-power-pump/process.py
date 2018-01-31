6dfca1487b4e6d53ee6d136e07945d2ba09cdf1b
Sean Dague <sean@dague.net>
2018-01-31 10:47:35 -0500

    add computing of co2 kg / kWh

--- images/ny-power-pump/process.py
+++ images/ny-power-pump/process.py
@@ -20,6 +20,37 @@ logging.basicConfig(
 
 HOST = os.environ.get("MQTT_HOST")
 
+# PWR in MWh
+# CO2 in Metric Tons
+
+FUEL_2016 = {
+    "Petroleum": {
+        "Power": 642952,
+        "CO2": 623836
+    },
+    "Natural Gas": {
+        "Power": 56793336,
+        "CO2": 26865277
+    }
+}
+
+# assume Dual Fuel systems consume 30% of state NG. That's probably low.
+FUEL_2016["Dual Fuel"] = {
+    "Power": (FUEL_2016["Petroleum"]["Power"] + (FUEL_2016["Natural Gas"]["Power"] * .3)),
+    "CO2": (FUEL_2016["Petroleum"]["CO2"] + (FUEL_2016["Natural Gas"]["CO2"] * .3)),
+}
+
+# Calculate CO2 per kWh usage
+
+def co2_for_fuel(fuel):
+    if fuel in FUEL_2016:
+        hpow = FUEL_2016[fuel]["Power"]
+        hco2 = FUEL_2016[fuel]["CO2"]
+        co2per = float(hco2) / float(hpow)
+        return co2per
+    else:
+        return 0.0
+
 def get_pass():
     with open("/etc/secret-volume/password") as f:
         return f.read()
@@ -55,18 +86,30 @@ def send_last_to_mqtt(data, last_sent=""):
         last = r[0]
 
     retval = None
+    kW = 0
+    co2 = 0
     for r in data:
         if r[0] == last and r[0] != last_sent:
             _LOGGER.info("Found new data to publish: %s", r)
             client.publish("ny-power/fuel-mix/{0}".format(r[2]),
                            json.dumps(
                                dict(ts=r[0], power=int(float(r[3])), units="kW")),
-                           qos=1)
+                           qos=1, retain=True)
             retval = r[0]
+            kW += float(r[3])
+            co2 += float(r[3]) * co2_for_fuel(r[2])
+
     _LOGGER.info("Last connect time is %s", retval)
+
+    if kW:
+        co2_per_kW = co2 / kW
+        client.publish("ny-power/co2",
+                       json.dumps(dict(ts=retval, emissions=co2_per_kW, units="kg / kWh")),
+                       qos=1, retain=True)
+
     if retval is not None:
         client.publish("ny-power/updated/fuel-mix",
-                       json.dumps(dict(ts=r[0])), qos=1, retain=True)
+                       json.dumps(dict(ts=retval)), qos=1, retain=True)
     client.disconnect()
     return retval
 
