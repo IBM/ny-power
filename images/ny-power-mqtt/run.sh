10f55c8245bc22979011864fdd0fa413a330c4ed
Sean Dague <sean@dague.net>
2018-02-09 07:41:39 -0500

    move mqtt pass to env

--- images/ny-power-mqtt/run.sh
+++ images/ny-power-mqtt/run.sh
@@ -2,10 +2,8 @@
 
 set -x -e
 
-PASS=$(cat /etc/secret-volume/password)
-
 touch /etc/mqtt_pass
-/usr/bin/mosquitto_passwd -b /etc/mqtt_pass pump ${PASS}
+/usr/bin/mosquitto_passwd -b /etc/mqtt_pass pump ${MQTT_PUMP_PASS}
 
 mkdir -p /shared/mqtt || /bin/true
 chown -R mosquitto.mosquitto /shared/mqtt
