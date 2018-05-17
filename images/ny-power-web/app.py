88b18b0394274f02afb46215a75fe2f7bca38c50
Sean Dague <sean@dague.net>
2018-05-17 10:01:33 -0400

    don't pump with app

--- images/ny-power-web/app.py
+++ images/ny-power-web/app.py
@@ -17,13 +17,6 @@ INFLUXDB_HOST = os.environ.get("INFLUXDB_HOST")
 MQTT_HOST = os.environ.get("MQTT_HOST")
 MQTT_PUMP_PASS = os.environ.get("MQTT_PUMP_PASS")
 
-# This signals that we've loaded new code, which means maybe the
-# client apps should reload
-_LOGGER.info("Try to send mqtt packet")
-publish.single("ny-power/application/webui", "reload", hostname=MQTT_HOST,
-               auth={"username": "pump", "password": MQTT_PUMP_PASS})
-_LOGGER.info("Should have been sent")
-
 @app.route("/")
 def index():
     return render_template("index.html", mqtt_host=MQTT_HOST)
