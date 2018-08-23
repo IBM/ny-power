c7fdf6a2443d9cdfca5a4e113628611b4142becc
Sean Dague <sean@dague.net>
2018-08-23 09:59:40 -0400

    fix time series

--- src/nypower/cmd/archive.py
+++ src/nypower/cmd/archive.py
@@ -35,6 +35,8 @@ def on_message(client, userdata, msg):
         # because of EST we really need to go back 28h from a UTC timestamp.
         since = "28h"
         series = influx.get_timeseries("co2_computed", since)
+        # change since back to 24h for the mqtt bus
+        since = "24h"
         client.publish("ny-power/archive/co2/%s" % since,
                        json.dumps(series),
                        qos=2, retain=True)
